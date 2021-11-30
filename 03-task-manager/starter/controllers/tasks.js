167b4303d7e206e74e158ce3029f9c570717a72c
JonnathanLenovo <andres.652@hotmail.com>
2021-11-30 11:29:00 -0500

    EditTask for PUT example

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -63,4 +63,22 @@ const deleteTask = async (req, res) => {
   res.send("delete task");
 };
 
-module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
+// with PUT method the expectation is to replace the item
+const editTask = async (req, res) => {
+    try {
+      const { id: taskID } = req.params;
+      const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+          new: true, // return the values of the new object
+          runValidators: true,
+          override: true // removes all fields that are not in the incoming json
+      });
+      if (!task) {
+        return res.status(404).json({ msg: `No task with id: ${taskID}` });
+      }
+      res.status(200).json({ task });
+    } catch (error) {
+      res.status(500).json({ msg: error });
+    }
+  };
+
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask, editTask };
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks')
+const { getAllTasks, createTask, getTask, updateTask, deleteTask, editTask } = require('../controllers/tasks')
 
 const router = express.Router()
 
 router.route('/').get(getAllTasks).post(createTask);
-router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask).put(editTask);
 
 module.exports = router;
