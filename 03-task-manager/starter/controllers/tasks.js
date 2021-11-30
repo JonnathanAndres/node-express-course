8c8c57e7d2eeab1f73d4ca8175bbd31f2d0b1115
JonnathanLenovo <andres.652@hotmail.com>
2021-11-30 11:04:01 -0500

    UpdateTask method

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -31,11 +31,35 @@ const getTask = async (req, res) => {
   }
 };
 
-const updateTask = (req, res) => {
-  res.send("udpate task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+        new: true, // return the values of the new object
+        runValidators: true
+    });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res) => {
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    // res.status(200).json({ task });
+    // res.status(200).send();
+    res.status(200).json({ task: null, status: "success" });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
   res.send("delete task");
 };
 
