7b27483ca24aaaa19e27929a5ea9c9cd96fb3a93
JonnathanLenovo <andres.652@hotmail.com>
2021-11-30 09:32:48 -0500

    Basic validation

--- 03-task-manager/starter/models/Task.js
+++ 03-task-manager/starter/models/Task.js
@@ -1,10 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // defines the structure for the document
 const TaskSchema = new mongoose.Schema({
-    name:String,completed:Boolean
-})
+  name: {
+    type: String,
+    required: [true, "must provide name"],
+    trim: true,
+    maxlength: [20, "name can not be more than 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
 
 // model is the interface to the database
 
-module.exports = mongoose.model('Task', TaskSchema)
+module.exports = mongoose.model("Task", TaskSchema);
