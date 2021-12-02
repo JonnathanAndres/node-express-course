463c01ce7339c68b6486b04dddd24473c2e516af
JonnathanLenovo <andres.652@hotmail.com>
2021-12-02 14:57:00 -0500

    Router setup

--- 04-store-api/starter/app.js
+++ 04-store-api/starter/app.js
@@ -1,10 +1,12 @@
 require("dotenv").config(); // access environment variables
+
 // async errors
 
 const express = require("express");
 const app = express();
 
-const connectDB = require('./db/connect')
+const connectDB = require("./db/connect");
+const productsRouter = require('./routes/products')
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
@@ -12,11 +14,12 @@ const errorMiddleware = require("./middleware/error-handler");
 // middleware
 app.use(express.json()); // just to not forgett it (will not be used in this project)
 
+// routes
 app.get("/", (req, res) => {
-  res.send(
-    '<h1>Store API <a href="/api/v1/products">products route</a></h1>'
-  );
+  res.send('<h1>Store API <a href="/api/v1/products">products route</a></h1>');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 // products route
 
@@ -28,7 +31,7 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connect to DB
-    await connectDB(process.env.MONGO_URI)
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening port ${port}...`));
   } catch (error) {
     console.log(error);
